2e3ab4f57e2e6e5e4c089d845402d50769bdebe8
Denis Lykov <cailean393@gmail.com>
2018-11-27 18:43:25 +0300

    change welcome font

--- util/welcome.js
+++ util/welcome.js
@@ -5,6 +5,6 @@ const asciifyPromise = util.promisify(asciify);
 
 module.exports = async function() {
   console.log('');
-  const ascii_image = await asciifyPromise('Electron-cli', { color: 'green', font: '3-d' });
+  const ascii_image = await asciifyPromise('Electron-cli', { color: 'green', font: 'big' });
   console.log(ascii_image);
 }
